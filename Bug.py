17c1e36f687a668fcf88a376d44148dc159f2ef3
HakanBue <h.buescher@outlook.de>
2024-06-16 23:09:46 +0200

    fixed naming conventions, mapping columns to numerical values

--- Bug.py
+++ Bug.py
@@ -2,21 +2,31 @@ import csv
 
 
 class Bugs:
-    def __init__(self, bug_id, creation_date, severity, status, discription, priority, pl_resolved_date, resolve_date,
-                 kategory, reproductionrate, sprints, user):
+    def __init__(self, bug_id, creation_date, severity, status, description, priority, pl_resolved_date, resolve_date,
+                 category, reproductionrate, sprints, user):
         self.bug_id = bug_id
         self.creation_date = creation_date
         self.severity = severity
         self.status = status
-        self.discription = discription
+        self.description = description
         self.priority = priority
         self.pl_resolve_date = pl_resolved_date
         self.resolve_date = resolve_date
-        self.kategory = kategory
+        self.category = category
         self.reproductionrate = reproductionrate
         self.sprints = sprints
         self.user = user
 
+# Macht aus Spalten wie z.B Priorität oder Schweregrad Nummern damit man danach sortieren kann
+def map_values(value):
+    mapping = {
+        'Niedrig': 0,
+        'Mittel': 1,
+        'Hoch': 2,
+        'Sehr hoch': 3,
+        'Kritisch': 4,
+    }
+    return mapping.get(value, value)
 
 def read_csv_to_bugs(csv_file_path, delimiter=';'):
     bugs_list = []
@@ -27,14 +37,14 @@ def read_csv_to_bugs(csv_file_path, delimiter=';'):
             bug = Bugs(
                 bug_id=row['Bug-ID'],
                 creation_date=row['Erstellungsdatum'],
-                severity=row['Schweregrad'],
+                severity=map_values(row['Schweregrad']),
                 status=row['Status'],
-                discription=row['Beschreibung'],
-                priority=row['Priorität'],
+                description=row['Beschreibung'],
+                priority=map_values(row['Priorität']),
                 pl_resolved_date=row['Geplantes Behebungsdatum'],
                 resolve_date=row['Tatsächliches Behebungsdatum'],
-                kategory=row['Kategorie'],
-                reproductionrate=row['Reproduktionsrate'],
+                category=row['Kategorie'],
+                reproductionrate=map_values(row['Reproduktionsrate']),
                 sprints=row['Voraussichtliche Sprints bis Behebung (in Wochen)'],
                 user=row['Beeinträchtigte Nutzer']
             )
@@ -49,4 +59,4 @@ bugs_list = read_csv_to_bugs(csv_file_path)
 
 # Überprüfung der ersten Einträge
 for bug in bugs_list[:5]:
-    print(vars(bug))
+    print(f'{vars(bug)} \n')
