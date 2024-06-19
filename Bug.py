19cf1d43a1eb468f6864c631dcde56edaa53bf15
aenju10 <142062782+aenju10@users.noreply.github.com>
2024-06-19 12:11:27 +0200

    anjas versuch

--- Bug.py
+++ Bug.py
@@ -1,21 +1,6 @@
 import csv
 from datetime import datetime
-
-class Bug:
-    def __init__(self, bug_id, creation_date, severity, status, description, priority, pl_resolved_date, resolve_date,
-                 category, reproductionrate, sprints, user):
-        self.bug_id = bug_id
-        self.creation_date = creation_date
-        self.severity = severity
-        self.status = status
-        self.description = description
-        self.priority = priority
-        self.pl_resolve_date = pl_resolved_date
-        self.resolve_date = resolve_date
-        self.category = category
-        self.reproductionrate = reproductionrate
-        self.sprints = sprints
-        self.user = user
+from sorter import insertionSort
 
 # Convert date to epoch time
 def date_to_epoch(date_str):
@@ -33,6 +18,8 @@ def map_values(value):
     }
     return mapping.get(value, value)
 
+    # Warum eigene Klasse implementieren, wenn es Dictionaries gibt?
+
 def read_csv_to_bugs(csv_file_path, delimiter=';'):
     bugs_list = []
 
@@ -42,20 +29,20 @@ def read_csv_to_bugs(csv_file_path, delimiter=';'):
             reproductionrate = map_values(row['Reproduktionsrate'])
             if isinstance(reproductionrate, str):
                 reproductionrate = float(reproductionrate.replace(',', '.'))
-            bug = Bug(
-                bug_id=row['Bug-ID'],
-                creation_date=date_to_epoch(row['Erstellungsdatum']),
-                severity=map_values(row['Schweregrad']),
-                status=row['Status'],
-                description=row['Beschreibung'],
-                priority=map_values(row['Priorität']),
-                pl_resolved_date=date_to_epoch(row['Geplantes Behebungsdatum']),
-                resolve_date=date_to_epoch(row['Tatsächliches Behebungsdatum']) if row['Tatsächliches Behebungsdatum'] else None,
-                category=row['Kategorie'],
-                reproductionrate=reproductionrate,
-                sprints=float(row['Voraussichtliche Sprints bis Behebung (in Wochen)'].replace(',', '.')),
-                user=int(row['Beeinträchtigte Nutzer'].replace('.', '').replace(',', ''))
-            )
+            bug = {
+                "bug_id":row['Bug-ID'],
+                "creation_date":date_to_epoch(row['Erstellungsdatum']),
+                "severity":map_values(row['Schweregrad']),
+                "status":row['Status'],
+                "description":row['Beschreibung'],
+                "priority":map_values(row['Priorität']),
+                "pl_resolved_date":date_to_epoch(row['Geplantes Behebungsdatum']),
+                "resolve_date":date_to_epoch(row['Tatsächliches Behebungsdatum']) if row['Tatsächliches Behebungsdatum'] else None,
+                "category":row['Kategorie'],
+                "reproductionrate":reproductionrate,
+                "sprints":float(row['Voraussichtliche Sprints bis Behebung (in Wochen)'].replace(',', '.')),
+                "user":int(row['Beeinträchtigte Nutzer'].replace('.', '').replace(',', ''))
+            }
             bugs_list.append(bug)
 
     return bugs_list
@@ -64,6 +51,12 @@ def read_csv_to_bugs(csv_file_path, delimiter=';'):
 csv_file_path = 'Bugreport_fixed_csv.csv'
 bugs_list = read_csv_to_bugs(csv_file_path)
 
+test1 = input("Erste Wahl: ")
+test2 = input("Zweite Wahl: ")
+
+#Insertion Sort Funktion mit den 2 zu sortierenden Feldern aufrufen --- Mit der GUI Verknüpfen
+insertionSort(bugs_list, test1, test2)
+
 # Check the first entries
-for bug in bugs_list[:5]:
-    print(f'{vars(bug)} \n')
+for bug in bugs_list:
+    print(bug[test1], bug[test2])
--- sorter.py
+++ sorter.py
@@ -1,4 +1,4 @@
-def insertionSort(arr):
+def insertionSort(arr, firstAtt, secondAtt):
     n = len(arr)
       
     # liste muss nicht sortiert werden, falls die liste leer ist oder es nur 1 element gibt
@@ -6,16 +6,17 @@ def insertionSort(arr):
         return  
  
     for i in range(1, n): # Wir fangen beim 2. element (Index 1) an 
-        key = arr[i]  
-        j = i-1
-        while j >= 0 and key < arr[j]:  # Werte die Größer sind werden eine position nach rechts verschoben
-            arr[j+1] = arr[j]  # Werte werden nach rechts verschoben
-            j -= 1
-        arr[j+1] = key  # Key wird in der richtigen Position eingefügt 
-    
+        for i in range(1, len(arr)):
+            key = arr[i][firstAtt]
+            second_key = arr[i][secondAtt]
+            j = i - 1
+            while j >= 0 and key > arr[j][firstAtt] or key == arr[j][firstAtt] and second_key > arr[j][secondAtt]:
+                arr[j+1], arr[j] = arr[j], arr[j+1]
+                j -= 1
+            arr[j+1][firstAtt] = key
 
 
-test_list = [73, 52, 66, 41, 12, 18, 4, 19]
+""" test_list = [73, 52, 66, 41, 12, 18, 4, 19]
 print(test_list)
 insertionSort(test_list)
-print(test_list)
+print(test_list) """
