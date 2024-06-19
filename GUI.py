22fb57373cb8847bbe791e010c4dc56ac9e8bf9f
Hakan Buescher <buescher@dvv.de>
2024-06-19 14:29:02 +0200

    todo, mit den gepassed kriterien sortieren, ans frontend zurück schicken und anzeigen

--- GUI.py
+++ GUI.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+from sorter import insertion_sort
+from Bug import read_csv_to_bugs
 
 app = Flask(__name__)
 CORS(app)
@@ -19,8 +21,18 @@ def home():
 def submit():
     data = request.json
     print(data)
-    # sorting_parameters = []
-    return jsonify(message=f'test')
+    sorting_parameters = data['sorting_parameters']
+
+
+    # Bugs einlesen
+    csv_file_path = 'Bugreport_fixed_csv.csv'
+    bugs_list = read_csv_to_bugs(csv_file_path)
+
+    for bug in bugs_list:
+        print(f'{vars(bug)} \n \n --------  ')
+    
+    
+    return jsonify(message=f'{data}')
 
 
 
